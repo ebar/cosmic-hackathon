ccaf63923bbd90fe67dfbc5671fc4c494b232cbb
Emily Barber <emily.barber@visualdna.com>
2014-05-31 12:25:24 +0100

    Added questionnaire JSON transformer

--- src/main/java/com/cosmic/questionnaire/QuestionnaireJsonTransformer.java
+++ src/main/java/com/cosmic/questionnaire/QuestionnaireJsonTransformer.java
@@ -1,5 +1,63 @@
 package com.cosmic.questionnaire;
 
-public class QuestionnaireJsonTransformer {
+import java.io.File;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class QuestionnaireJsonTransformer {
+ 
+	@SuppressWarnings("unchecked")
+	public static JSONObject createJson() {
+		
+		JSONObject questionnaireObject = new JSONObject();
+		
+		File questionnaireDirectory = new File("feedback/questionnaire");
+		
+		if (questionnaireDirectory.isDirectory()) {
+			String[] categories = questionnaireDirectory.list();
+			JSONArray categoryArray = new JSONArray();
+			for (String category : categories) {
+				File categoryDirectory = new File(questionnaireDirectory, category);
+				JSONObject categoryObject = new JSONObject();
+				if (categoryDirectory.isDirectory()) {
+					String[] questions = categoryDirectory.list();
+					JSONArray allQuestionsArray = new JSONArray();
+					for (String question : questions) {
+						File questionDirectory = new File(categoryDirectory, question);
+						JSONObject questionObject = new JSONObject();
+						if (questionDirectory.isDirectory()) {
+							JSONArray optionsArray = new JSONArray();
+							String[] options = questionDirectory.list();
+							for (String option : options) {
+								File optionFile = new File(questionDirectory, option);
+								JSONObject optionObject = new JSONObject();
+								optionObject.put("imageUrl", optionFile.getAbsolutePath());
+								optionsArray.add(optionObject);
+							}
+							questionObject.put("title", questionDirectory.getName());
+							questionObject.put("options", optionsArray);
+							allQuestionsArray.add(questionObject);
+						}
+						
+					}
+					categoryObject.put("category", categoryDirectory.getName());
+					categoryObject.put("questions", allQuestionsArray);
+					categoryArray.add(categoryObject);
+					
+				}
+				
+			}
+			questionnaireObject.put("categories", categoryArray);
+		}
+		return questionnaireObject;
+		
+	}
+
+	
+	public static void main(String[] args) {
+		System.out.println(createJson());
+	}
+
+	
 }
